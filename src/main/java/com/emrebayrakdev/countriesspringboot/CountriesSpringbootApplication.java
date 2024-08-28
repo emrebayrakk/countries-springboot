147335417fd037e75783203893e0ad2f29efc54a
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2024-08-28 18:05:49 +0300

    first_commit

---
+++ src/main/java/com/emrebayrakdev/countriesspringboot/CountriesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.emrebayrakdev.countriesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emrebayrakdev/countriesspringboot/CountriesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrebayrakdev.countriesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
