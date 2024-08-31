e41a86e33000c65598b250a99a9514d0fc2e5645
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2024-08-31 18:49:40 +0300

    bearer token configuration with swagger configuration

--- src/main/java/com/emrebayrakdev/countriesspringboot/config/SwaggerConfiguration.java
+++ src/main/java/com/emrebayrakdev/countriesspringboot/config/SwaggerConfiguration.java
@@ -1,9 +1,12 @@
 package com.emrebayrakdev.countriesspringboot.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public OpenAPI defineOpenApi() {
+        final String securitySchemeName = "bearerAuth";
         Server server = new Server();
         server.setUrl("http://localhost:8080/api");
         server.setDescription("Development");
@@ -28,6 +32,16 @@ public class SwaggerConfiguration {
                 .version("1.0.0")
                 .description("Spring Boot Learning..")
                 .contact(myContact);
-        return new OpenAPI().info(information).servers(List.of(server));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new  io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(information)
+                .servers(List.of(server));
     }
 }
